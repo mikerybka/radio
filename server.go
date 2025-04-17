b5f6d7f917d41182b16ed12e189ff79bc7553a76
Mike Rybka <merybka@gmail.com>
2025-04-17 08:14:34 -0400

    Remove HTML

--- server.go
+++ server.go
@@ -1,7 +1,6 @@
 package radio
 
 import (
-	_ "embed"
 	"net/http"
 	"os"
 	"os/exec"
@@ -9,7 +8,7 @@ import (
 
 func NewServer() http.Handler {
 	radio := http.NewServeMux()
-	radio.HandleFunc("GET /live.mp3", func(w http.ResponseWriter, r *http.Request) {
+	radio.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "audio/mpeg")
 		cmd := exec.Command("ffmpeg",
 			"-f", "pulse",
@@ -23,18 +22,5 @@ func NewServer() http.Handler {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
-	radio.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "text/html")
-		w.Write([]byte(`<div><audio controls autoplay>
-			<source src="/live.mp3" type="audio/mpeg">
-			Your browser does not support the audio element.
-		</audio></div><p>Refresh if the stream stops.</p>`))
-	})
-	radio.HandleFunc("GET /play", func(w http.ResponseWriter, r *http.Request) {
-		w.Write(playHTML)
-	})
 	return radio
 }
-
-//go:embed play.html
-var playHTML []byte
